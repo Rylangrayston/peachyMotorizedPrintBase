24da6a4a7247957a697d30c9a47a459b810e45ea
rylangrayston <rylangrayston@gmail.com>
2016-03-07 04:20:03 -0500

    added the stepper motor code along with how it is updated and choses direction.

---
+++ peachyMotorizedPrintBase/stepper.h
@@ -0,0 +1,160 @@
+#include "Arduino.h"
+
+    /*
+       BYJ48 Stepper motor code
+       Connect :
+       IN1 >> D8
+       IN2 >> D9
+       IN3 >> D10
+       IN4 >> D11
+       VCC ... 5V Prefer to use external 5V Source
+       Gnd
+       written By :Mohannad Rawashdeh
+      http://www.instructables.com/member/Mohannad+Rawashdeh/
+         28/9/2013
+      */
+
+    #define IN1  2
+    #define IN2  3
+    #define IN3  4
+    #define IN4  5
+
+int microSecondsBetweenSteps = 4000;
+unsigned long currentMicros = micros();
+unsigned long timeOfLastStep;
+unsigned long timeOfNextStep;
+boolean stepperBusy = false;
+int updateStepperCallsPerStepCount = 0;
+int updateStepperCallsPerStep = 0;
+boolean Direction = true;
+
+// pins
+#define upperLimitPin 12
+#define DOWN  true
+#define UP  false 
+
+    
+unsigned long stepperCurrentStepPos = 5000000; 
+unsigned long stepperTargetStepPos = 5000000;
+int Steps = 0;
+int pos = 0;
+
+     
+
+  
+  
+//  }
+//  updateStepperCallsPerStepCount++; 
+
+////////////////////////////////////////////////////////////// 
+    
+//void updateStepper(){
+//  if (steps_left > 0)
+//  {
+//    stepper(1); 
+//    steps_left--;
+//    stepperBusy = true;
+//  }
+//  else{stepperBusy = false;}
+//}
+    
+    void SetDirection(){
+    if(Direction==true){ Steps++; pos++;}
+    if(Direction==false){ Steps--; pos--;}
+    if(Steps>7){Steps=0;}
+    if(Steps<0){Steps=7; }
+    } 
+    
+
+    void stepper(int xw){
+      for (int x=0;x<xw;x++){
+    switch(Steps){
+       case 0:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, HIGH);
+       break; 
+       case 1:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, HIGH);
+       break; 
+       case 2:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 3:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 4:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 5:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+         case 6:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 7:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, HIGH);
+       break; 
+       default:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+    }
+    
+    
+    SetDirection();
+    }
+    } 
+    
+    
+
+    
+    
+    void updateStepper(){
+    if (stepperCurrentStepPos < stepperTargetStepPos)
+    {
+      Direction = DOWN;
+      stepperBusy = true;
+      stepper(1);
+      stepperCurrentStepPos += 1;
+    }
+    
+    else if (stepperCurrentStepPos > stepperTargetStepPos)
+    {
+      Direction = UP;
+      stepperBusy = true;
+      stepper(1);
+      stepperCurrentStepPos -= 1;
+      
+    }
+    
+    else 
+    {
+      stepperBusy = false;
+    }    
+    }
+
