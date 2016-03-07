417e4ca37b7eba81559a4082d77655da1f3e9d31
rylangrayston <rylangrayston@gmail.com>
2016-03-07 04:20:54 -0500

    you will need this change for the last 2 commits to work

--- peachyMotorizedPrintBase/peachyMotorizedPrintBase.ino
+++ peachyMotorizedPrintBase/peachyMotorizedPrintBase.ino
@@ -1,130 +1,15 @@
-class FlagAtFrequency
-{
-  float flagFrequency;
-  unsigned long microSecondsBetweenFlags; 
-  //unsigned long timeOfLastFlag;
-  unsigned long timeOfNextFlag;
-  int timesCalledSinceLastFlag;
-  int timesCalledBetweenFlags;
-  unsigned long totalFlagsSet;
-  unsigned long currentMicroSeconds;
-
-  public:
-  FlagAtFrequency(float frequency, unsigned long microSeconds) 
-  {
-    flagFrequency = frequency;
-    microSecondsBetweenFlags = microSeconds;
-    currentMicroSeconds = micros(); 
-    //timeOfLastFlag = currentMicroSeconds;
-    timeOfNextFlag = 0;
-    timesCalledSinceLastFlag = 0;
-    timesCalledBetweenFlags = 0;
-    totalFlagsSet = 0;
-  }
-  
-  boolean flag()
-  {
-    unsigned long currentMicroSeconds = micros();
-    if (currentMicroSeconds >= timeOfNextFlag)
-    {
-      timeOfNextFlag += microSecondsBetweenFlags;
-      timesCalledBetweenFlags = timesCalledSinceLastFlag;
-      timesCalledSinceLastFlag = 0;
-      totalFlagsSet += 1;
-      Serial.println(timeOfNextFlag);
-      
-      return true;
-    }
-    else{return false;}
-      
-  }
- int callsPerFlag()
- {
-   return timesCalledBetweenFlags;
- }
- 
- unsigned long totalFlags()
- {
-   return totalFlagsSet;
- }
- 
-};
-
-
-
-
+//#include "Arduino.h"
+#include "Flager.h"
+#include "stepper.h"
 
 ////////////////////////////////////////////////////////////////////////////
-int microSecondsBetweenSteps = 4000;
-unsigned long currentMicros = micros();
-unsigned long timeOfLastStep;
-unsigned long timeOfNextStep;
-boolean stepperBusy = false;
-int updateStepperCallsPerStepCount = 0;
-int updateStepperCallsPerStep = 0;
-boolean stepperDirection = true;
 
-// pins
-#define upperLimitPin 12
-#define DOWN  true
-#define UP  false 
-
-#define stepperCoil1Pin  2
-#define stepperCoil2Pin  3
-#define stepperCoil3Pin  4
-#define stepperCoil4Pin  5
-
-unsigned long stepperCurrentStepPos = 0; 
-unsigned long stepperTargetStepPos = 0;
-
-
-
-
-
-
-void updateStepper(){
-//  currentMicros = micros();
-//  if (currentMicros - timeOfLastStep >= timeOfNextStep)
-//  {
-//    timeOfLastStep = currentMicros;
-//    timeOfNextStep = timeOfLastStep + microSecondsBetweenSteps;
-//    updateStepperCallsPerStep = updateStepperCallsPerStepCount;
-//    updateStepperCallsPerStepCount = 0;
-    
-  
-    if (stepperCurrentStepPos < stepperTargetStepPos)
-    {
-      stepperDirection = DOWN;
-      stepperBusy = true;
-      
-    }
-    
-    else if (stepperCurrentStepPos > stepperTargetStepPos)
-    {
-      stepperDirection = UP;
-      stepperBusy = true;
-      
-    }
-    
-    else 
-    {
-      stepperBusy = false;
-    }    
-  
-  
-//  }
-//  updateStepperCallsPerStepCount++; 
+void printStatus()
+{
+ Serial.println(stepperCurrentStepPos);
+ Serial.println(stepperTargetStepPos);
+ Serial.println(stepperCurrentStepPos - stepperTargetStepPos); 
 }
-
-
-
-
-
-
-
-
-
-
 
 
 ///// flags ////
@@ -132,7 +17,7 @@ void updateStepper(){
 FlagAtFrequency stepperMotorFlager(0.0, microSecondsBetweenSteps);
 boolean stepperMotorFlag = false;
 
-FlagAtFrequency lowPriorityFlager(0.0, 1000000);
+FlagAtFrequency lowPriorityFlager(0.0, 200000);
 boolean lowPriorityFlag = false;
 
 
@@ -158,7 +43,9 @@ void doHighPriorityWork()
 
 void doLowPriorityWork()
 {
-  
+  Serial.println(stepperTargetStepPos);  
+  if (stepperBusy == false){ stepperTargetStepPos +=0;}
+  printStatus();
 }
 
 void setup()
@@ -176,6 +63,8 @@ void loop()
   {
     updateLowPriorityFlags();
     doLowPriorityWork();
+    
+    lowPriorityFlag = false;
   } 
 }
   
